1710e100d5754c1f734f04ed1dbda93b109af90b
Lahrach Ilias <lial@192.168.0.105>
2022-01-11 07:04:57 +0100

    Express Heroku

--- src/app/shared/assignments.service.ts
+++ src/app/shared/assignments.service.ts
@@ -14,8 +14,8 @@ export class AssignmentsService {
 
   constructor(private loggingService: LoggingService, private http:HttpClient) { }
 
-  url = "http://localhost:8010/api/assignments/";
-  //url = "https://api-emsi-rabat-2022.herokuapp.com/api/assignments";
+  //returnurl = "http://localhost:8010/api/assignments/";
+  url = "https://assignment-miage-backend.herokuapp.com/api/assignments/";
 
   getAssignments():Observable<Assignment[]> {
     return this.http.get<Assignment[]>(this.url);
